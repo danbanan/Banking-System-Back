90adf8783145fbb93c69a1c24fd053e0a4e1244a
danrachou <dan.rachou@gmail.com>
2019-05-08 11:59:38 -0700

    populate customer table

--- db/sql/customer-sql.js
+++ db/sql/customer-sql.js
@@ -21,11 +21,17 @@ const populate = {
         state, zip, phone, email_address)
     VALUES
     ('317-14-4057', 'Jordan', 'Bell', '9188 Young St.', 'Panorama City', 'CA',
-     '91402', '202-555-0163', 'jordan.bell@warriors.com'),
-    ('249-89-9093', 'Andrew', 'Bogut', '819 North Pheasant Ave.', 'Rancho Cucamonga', 
-     'CA', '91730', '202-555-0153', 'andrew.bogut@warriors.com'),
+    '91402', '202-555-0163', 'jordan.bell@warriors.com'),
+    ('249-89-9093', 'Andrew', 'Bogut', '819 North Pheasant Ave.', 
+    'Rancho Cucamonga', 'CA', '91730', '202-555-0153', 'andrew.bogut@warriors.com'),
     ('192-40-8735', 'Quinn', 'Cook', '9004 Clinton Street', 'Vallejo', 'CA', 
-     '94591', '202-555-0160', 'quinn.cook@warriors.com');
+    '94591', '202-555-0160', 'quinn.cook@warriors.com'),
+    ('627-10-7680', 'DeMarcus', 'Cousins, '556 Grandrose St.', 'El Cajon', 'CA',
+    '92020', '202-555-0128', 'demarcus.cousins@warriors.com'),
+    ('388-19-3667', 'Stephen', 'Curry', '46 Wakehurst Dr.', 'Lompoc', 'CA', 
+    '93436', '202-555-0199', 'stephen.curry@warriors.com'),
+    ('608-82-2944', 'Marcus', 'Derrickson', '39 High Ridge Rd.', 'Carmichael',
+    'CA', '95608', '202-555-0133', 'marcus.derrickson@warriors.com);
     `
 }
 
